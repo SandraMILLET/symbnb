20261a577ba0f9e505e7ec5eca2ea0f09f9a3f4d
SandraMILLET <sandra.millet11@gmail.com>
2020-10-12 16:05:59 +0200

    mise en forme form + insertion input bouton + et suppression

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,8 +3,12 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Entity\Image;
+use App\Form\AnnonceType;
 use App\Repository\AdRepository;
+use Doctrine\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -22,9 +26,42 @@ class AdController extends AbstractController
         ]);
     }
 
+    /**
+     * permet de créer une annonce
+     *
+     * @Route("/ads/new", name="ads_create")
+     *
+     * @return Response
+     */
+    public function create(Request $request)
+    {
+        $ad = new Ad();
+
+        $form = $this->createForm(AnnonceType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid()) {
+            $manager=$this->getDoctrine()->getManager();
+            $manager->persist($ad);
+            $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée ! "
+        );
+
+            return $this->redirectToRoute('ads_show', [
+                'slug' => $ad->getSlug()
+            ]);
+        }
+        return $this->render('ad/new.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
 
     /**
-     * Permet d'afficher une seule annonce
+     *
      * @Route("/ads/{slug}", name="ads_show")
      * @return Response
      */
@@ -32,10 +69,10 @@ class AdController extends AbstractController
     {
         //je récupère l'annonce qui correspond au slug
         //$ad = $repo->findOneBySlug($slug);
-
         return $this->render('ad/show.html.twig', [
             'ad' => $ad
         ]);
     }
+
 
 }
