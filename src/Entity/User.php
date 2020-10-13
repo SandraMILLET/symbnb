44646c4fced08fb6014ea2f7f8dc3d97c2d656bc
SandraMILLET <sandra.millet11@gmail.com>
2020-10-13 15:37:38 +0200

    login

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,12 +7,13 @@ use Cocur\Slugify\Slugify;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
  * @ORM\HasLifecycleCallbacks()
  */
-class User
+class User implements UserInterface
 {
     /**
      * @ORM\Id
@@ -215,4 +216,21 @@ class User
 
         return $this;
     }
+
+    public function getRoles(){
+        return ['ROLE_USER'];
+    }
+
+    public function getPassword(){
+        return $this->hash;
+    }
+
+    public function getSalt(){ }
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function eraseCredentials(){ }
 }
