78a036b9da7f32b927656819e7d70bdaced5ea3d
SandraMILLET <sandra.millet11@gmail.com>
2020-10-16 15:03:00 +0200

    securisation

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -7,6 +7,8 @@ use App\Entity\Image;
 use App\Form\AnnonceType;
 use App\Repository\AdRepository;
 use Doctrine\Persistence\ObjectManager;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Security;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -30,6 +32,7 @@ class AdController extends AbstractController
      * permet de créer une annonce
      *
      * @Route("/ads/new", name="ads_create")
+     * @IsGranted("ROLE_USER")
      *
      * @return Response
      */
@@ -70,6 +73,7 @@ class AdController extends AbstractController
     /**
      * permet d'afficher le formulaire d'édition
      * @Route("ads/{slug}/edit", name="ads_edit")
+     * @Security("is_granted('ROLE_USER') and user === ad.getAuthor()", message="Cette annonce ne vous appartient pas, vous ne pouvez pas la modifier")
      * @return Response
      */
     public function edit(Ad $ad, Request $request)
@@ -95,27 +99,44 @@ class AdController extends AbstractController
                 'slug' => $ad->getSlug()
             ]);
         }
-            return $this->render('ad/edit.html.twig', [
-                'form' => $form->createView(),
-                'ad' => $ad
-            ]);
-        }
-
-
-        /**
-         *
-         * @Route("/ads/{slug}", name="ads_show")
-         * @return Response
-         */
-        public
-        function show(Ad $ad)
-        {
-            //je récupère l'annonce qui correspond au slug
-            //$ad = $repo->findOneBySlug($slug);
-            return $this->render('ad/show.html.twig', [
-                'ad' => $ad
-            ]);
-        }
-
-
+        return $this->render('ad/edit.html.twig', [
+            'form' => $form->createView(),
+            'ad' => $ad
+        ]);
     }
+
+
+    /**
+     *
+     * @Route("/ads/{slug}", name="ads_show")
+     * @return Response
+     */
+    public
+    function show(Ad $ad)
+    {
+        //je récupère l'annonce qui correspond au slug
+        //$ad = $repo->findOneBySlug($slug);
+        return $this->render('ad/show.html.twig', [
+            'ad' => $ad
+        ]);
+    }
+    /**
+     *Permet de supprimer une annonce
+     * @Route("/ads/{slug}/delete", name="ads_delete")
+     * @Security("is_granted('ROLE_USER') and user == ad.getAuthor()", message="Vous n'avez pas le droit d'accéder à cette ressource")
+     * @param Ad $ad
+     * @return Response
+     */
+    public function delete(Ad $ad){
+        $manager = $this->getDoctrine()->getManager();
+        $manager->remove($ad);
+        $manager->flush();
+        $this->addFlash(
+            'success',
+            "L'annonce <strong>{$ad->getTitle()}</strong> a bien été supprimée !"
+        );
+return $this->redirectToRoute("ads_index");
+    }
+
+
+}
