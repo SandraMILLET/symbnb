8728043fd8d7b5f520a6e194de7076a82acf9f5a
SandraMILLET <sandra.millet11@gmail.com>
2020-10-21 16:14:23 +0200

    booking

---
+++ src/Controller/BookingController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Ad;
+use App\Entity\Booking;
+use App\Form\BookingType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+
+
+class BookingController extends AbstractController
+{
+    /**
+     * @Route("/ads/{slug}/book", name="booking_create")
+     * @IsGranted("ROLE_USER")
+     */
+    public function book(Ad $ad, Request $request)
+    {
+        $booking = new Booking();
+        $form = $this->createForm(BookingType::class, $booking);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $this->getUser();
+
+            $booking->setBooker($user)
+                ->setAd($ad);
+
+            $manager = $this->getDoctrine()->getManager();
+            $manager->persist($booking);
+            $manager->flush();
+
+            return $this->redirectToRoute('booking_show', ['id' => $booking->getId()]);
+
+        }
+        return $this->render('booking/book.html.twig', [
+           'ad' =>$ad,
+            'form'=> $form->createView()
+        ]);
+    }
+
+    /**
+     * Permet d'afficher ue réservation
+     * @Route("/booking/{id}", name="booking_show")
+     * @param Booking $booking
+     * @return Response
+     */
+    public function show(Booking $booking){
+    return $this->render('booking/show.html.twig', [
+        'booking'=> $booking
+    ]);
+    }
+}
